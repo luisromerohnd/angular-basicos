ef6c25bc4154fab7da9799bd929d5ac3aa7263e5
Luis Romero <luisarnaldo11@gmail.com>
2021-02-07 19:00:54 -0500

    Primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styles: [
+  ]
+})
+export class ListadoComponent{
+
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitan America'];
+  heroeBorrado: string = '';
+
+  borrarHeroe() {
+    const heroeBorrado = this.heroes.shift() || '';
+    this.heroeBorrado = heroeBorrado;
+  }
+
+}
